da16c7a702ac851440b4afb29762f0e0a0b19876
Programador4 <programador4@tair.pe>
2026-06-15 09:00:55 -0500

    feat: implementation of endpoint versioning

--- server.js
+++ server.js
@@ -16,19 +16,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const { IP_LOCAL } = require("./src/shared/conf.js");
 const authenticateToken = require("./src/shared/middleware/jwt-valid.js");
-
-// RUTAS DE API
-const authRoutes = require("./src/app/auth/auth.routes.js");
-const clientRoutes = require("./src/app/client/client.routes.js");
-const contractRoutes = require("./src/app/contract/contract.routes.js");
-const documentRoutes = require("./src/app/document/document.routes.js");
-const leasingRoutes = require("./src/app/leasing/leasing.routes.js");
-const vehicleRoutes = require("./src/app/vehicle/vehicle.routes.js");
-const operationRoutes = require("./src/app/operation/operation.routes.js");
-const fileRoutes = require("./src/app/file/file.routes.js");
-const modelRoutes = require("./src/app/model/model.routes.js");
-const reportRoutes = require("./src/app/report/report.routes.js");
-const userRoutes = require("./src/app/user/user.routes.js");
+const routerV1 = require("./src/shared/routes/v1.js")
 
 const app = express();
 const port = process.env.PORT ?? "3000";
@@ -130,17 +118,7 @@ app.use(
 );
 
 // Rutas de api
-app.use(authRoutes);
-app.use(clientRoutes);
-app.use(contractRoutes);
-app.use(documentRoutes);
-app.use(leasingRoutes);
-app.use(vehicleRoutes);
-app.use(operationRoutes);
-app.use(fileRoutes);
-app.use(modelRoutes);
-app.use(reportRoutes);
-app.use(userRoutes);
+app.use("/api/v1", routerV1);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
